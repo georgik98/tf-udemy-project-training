05c45fbdd82d93f3439119c9f0a96dadafce92a7
Georgi Kolev <georgipetrovkolev1998@gmail.com>
2024-07-16 15:21:30 +0300

    backend gcp

--- main.tf
+++ main.tf
@@ -2,10 +2,17 @@ resource "google_storage_bucket" "default-bucket" {
   name          = "${var.project_id}-bucket-tfstate"
   project = "tf-udemy-project-training"
   location      = "EU"
-  force_destroy = false
+  force_destroy = falsee
   storage_class = "STANDARD"
 
   versioning {
     enabled = true
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "tf-udemy-project-training-bucket-tfstate"
+    prefix = "terraform-state"
   }
 }
